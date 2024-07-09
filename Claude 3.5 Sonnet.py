8585c0409dd1fcbe6a836a4a4550ee02198e9591
huenchunfan <1453420210@qqm.com>
2024-07-09 10:07:41 +0800

    add a new AI model

--- Claude 3.5 Sonnet.py
+++ Claude 3.5 Sonnet.py
@@ -1,22 +1,3 @@
-# import openai
-#
-# client = openai.OpenAI(
-#     base_url='https://api.openai-proxy.org/anthropic/v1/messages',
-#     api_key='sk-QNLQxq0rkkZcba18VLM80z1eguzLr3kbGua0WWsqpHjGl13E',
-# )
-#
-# response = client.chat.completions.create(
-#     messages=[
-#         {
-#             "role": "user",
-#             "content": "hi",
-#             "max_tokens": "3000",
-#         }
-#     ],
-#     model="claude-3-sonnet-20240229",
-# )
-#
-# print(response)
 import requests
 
 url = 'https://api.openai-proxy.org/anthropic/v1/messages'
